2069f3e023fbc44121bbc0295680f68e551374c7
Md. Anowar Kabir <35701531+AnowarKabir@users.noreply.github.com>
2024-09-30 18:54:49 +0600

    Update file.js
    choice update

--- Quiz/file.js
+++ Quiz/file.js
@@ -15,21 +15,21 @@ var questions = [
         "B"
     ],
     [
-        "3. What will be the result of the following code: x = 'Welcome' print(x[3])",
+        "3. What will be the result of the following code: x = 'Welcome'; print(x[3])",
         "A) Wel",
         "B) c",
         "C) Welcome Welcome Welcom",
         "B"
     ],
     [
-        "4. What will be the result of the following syntax: x = 5     x += 6       print(x)",
+        "4. What will be the result of the following syntax: x = 5; x += 6; print(x)",
         "A) 5",
         "B) 6",
         "C) 11",
         "C"
     ],
     [
-        "5. What will be the result of the following syntax: mylist = ['apple', 'banana', 'cherry']     print(mylist[-1])",
+        "5. What will be the result of the following syntax: mylist = ['apple', 'banana', 'cherry']; print(mylist[-1])",
         "A) apple",
         "B) banana",
         "C) cherry",
@@ -79,40 +79,49 @@ var questions = [
     ]
 ];
 
-function _(x){
-	return document.getElementById(x);
+function _(x) {
+    return document.getElementById(x);
 }
-function renderQuestion(){
-	test = _("test");
-	if(pos >= questions.length){
-		test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-		_("test_status").innerHTML = "Test Completed";
-		pos = 0;
-		correct = 0;
-		return false;
-	}
-	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
-	question = questions[pos][0];
-	chA = questions[pos][1];
-	chB = questions[pos][2];
-	chC = questions[pos][3];
-	test.innerHTML = "<h3>"+question+"</h3>";
-	test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
-	test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
-	test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br><br>";
-	test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+
+function renderQuestion() {
+    test = _("test");
+    if (pos >= questions.length) {
+        test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
+        _("test_status").innerHTML = "Test Completed";
+        pos = 0;
+        correct = 0;
+        return false;
+    }
+    _("test_status").innerHTML = "Question " + (pos + 1) + " of " + questions.length;
+    question = questions[pos][0];
+    chA = questions[pos][1];
+    chB = questions[pos][2];
+    chC = questions[pos][3];
+    test.innerHTML = "<h3>" + question + "</h3>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'> " + chA + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'> " + chB + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'> " + chC + "<br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
-function checkAnswer(){
-	choices = document.getElementsByName("choices");
-	for(var i=0; i<choices.length; i++){
-		if(choices[i].checked){
-			choice = choices[i].value;
-		}
-	}
-	if(choice == questions[pos][4]){
-		correct++;
-	}
-	pos++;
-	renderQuestion();
+
+function checkAnswer() {
+    choices = document.getElementsByName("choices");
+    choice = null; // Initialize choice
+    for (var i = 0; i < choices.length; i++) {
+        if (choices[i].checked) {
+            choice = choices[i].value;
+            break; // Exit loop once a choice is made
+        }
+    }
+    if (choice) {
+        if (choice === questions[pos][4]) {
+            correct++;
+        }
+        pos++;
+        renderQuestion();
+    } else {
+        alert("Please select an answer before submitting.");
+    }
 }
+
 window.addEventListener("load", renderQuestion, false);
